df3f289b0d326d128c6dc84d85eda5c1bc1c50c9
brahmikumar <ajayit786@gmail.com>
2021-12-29 16:53:10 +0530

    Add files via upload

---
+++ common/src/login/screens/login.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class Login extends StatefulWidget {
+  const Login({Key? key}) : super(key: key);
+
+  @override
+  _LoginState createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Login"),
+      ),
+      body: Center(
+        child: Container(
+          child: const Text("Login Page"),
+        ),
+      )
+    );
+  }
+}

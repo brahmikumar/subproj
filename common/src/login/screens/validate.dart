41ee67d0f22e033704c9e84e8272679085f0a835
brahmikumar <ajayit786@gmail.com>
2021-12-29 17:00:34 +0530

    subproj

---
+++ common/src/login/screens/validate.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Validate extends StatelessWidget {
+  const Validate({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
